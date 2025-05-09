48f3077e2caeb6a4b5f1188af0ed5304047d27d0
AlokzEpep <alokzgntng@gmail.com>
2025-05-09 13:09:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function halo() {
+  const output = document.getElementById("output");
+  output.innerHTML = "Xzo×Cheat youtuber pemula yg gak jelas banget, bantu subscribe ya..<br>Linkya di bawah";
+  output.classList.add("fade-in");
+  document.getElementById("btn2").style.display = "inline-block";
+}
+
+function tulisanBaru() {
+  const teksBaru = document.getElementById("teksBaru");
+  teksBaru.innerHTML = "buat jaga-jaga kalo di hapus yt :v";
+  teksBaru.classList.add("fade-in");
+
+  // Menampilkan tombol untuk membuka URL
+  document.getElementById("linkContainer").style.display = "block";
+}
